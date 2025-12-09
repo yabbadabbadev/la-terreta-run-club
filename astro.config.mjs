ff0a9a7753aceecac9173e24409df7bacdc309fa
Alex Fuentes <alfupe@gmail.com>
2025-12-10 00:07:45 +0100

    remove compressHTML from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-    // Reemplaza con la URL de tu GitHub Pages (usuario.github.io)
-    site: 'https://alfupe.github.io',
-    // Reemplaza con el nombre exacto de tu repositorio
-    base: '/la-terreta-run-club',
-});
+  // Reemplaza con la URL de tu GitHub Pages (usuario.github.io)
+  site: 'https://alfupe.github.io',
+  // Reemplaza con el nombre exacto de tu repositorio
+  base: '/la-terreta-run-club',
+  compressHTML: false,
+})
